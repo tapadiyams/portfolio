700463f5bf90e896244fac099bc41c39eb0ef5f1
Shubham Tapadiya <56501315+TapadiyaMS@users.noreply.github.com>
2023-04-16 17:31:01 -0400

    Update data.js
    Update content of project descriptions

--- src/data.js
+++ src/data.js
@@ -19,7 +19,7 @@ export const experiences = [
 
 export const projects = [
     {
-      title: "Road Symbols Detection",
+      title: "Road Symbol Recognition: A High-Precision Python Application for Identifying Road Symbols Using Image Processing Techniques with OpenCV",
       subtitle: "OpenCV Library, Python",
       description:"I developed a Python application that uses image processing techniques to identify road symbols with a high precision rate of 85%. I utilized OpenCV for image transformations, including masking techniques using bitwise operations to focus on the important parts of the image. The processed image was then compared with a set of road symbol images to accurately classify the label of the road symbol. This application has the potential to contribute to maintaining safe driving conditions by identifying road symbols effectively and efficiently.",
       image: "./Images/Projects/road_symbol_detection.jpg",
@@ -33,14 +33,14 @@ export const projects = [
       link: "https://github.com/TapadiyaMS/linkedin-clone",
     },
     {
-      title: "",
-      subtitle: "Reactjs, Firebase",
+      title: "Showcase Me: A Portfolio Website Built with ReactJS and Tailwind CSS",
+      subtitle: "Reactjs, CSS, Netlify",
       description:"Created a portfolio website to showcase my skills and work, utilizing ReactJS and Tailwind CSS for building the user interface. The use of reusable components in React allows for easy composition of landing page sections and potential future feature additions, such as a blog. Tailwind CSS is used for styling, allowing for professional and customizable appearance with combined classnames on React elements. The website is hosted on Netlify, a free service that serves the project on a custom domain with the help of a content delivery network (CDN), ensuring fast and efficient loading for users.",
       image: "./Images/Projects/portfolio.jpg",
       link: "https://github.com/TapadiyaMS/portfolio",
     },
     {
-      title: "Hotel Accreditation and Sentimental Analysis with Machine Learning",
+      title: "Analyzing Guest Reviews: A Comparative Study of MLP, SVM, and KNN with GridSearchCV and Sentiment Analysis using VaderSentiment Library",
       subtitle: "Python-Google Colaboratory, NoSQL",
       description:"An application was constructed to perform Multi-layer Perceptron, Support Vector Machine, and K-Nearest Neighbor analysis on guest reviews stored in a NoSQL database. GridSearchCV was implemented to identify the optimal parameters for the MLP algorithm, resulting in an accuracy rate of 92%. Additionally, the VaderSentiment Library was utilized to perform sentiment analysis on the guest reviews, providing valuable insights into the sentiment expressed in the reviews.",
       image: "./Images/Projects/sentimental_analysis.jpg",
